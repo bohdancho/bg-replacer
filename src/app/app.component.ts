740bef019bdd93d56d3b54b3b79096f0d1616b27
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-23 15:08:20 +0100

    add frontend file upload with preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router'
 })
 export class AppComponent {
   title = 'bg-replacer'
+  imageUrl: string | ArrayBuffer | null = null
+  file: File | null = null
+
+  onFileChange(event: Event) {
+    const files = (event.target as HTMLInputElement).files
+    if (!files || files.length === 0) return
+
+    const reader = new FileReader()
+    reader.readAsDataURL(files[0])
+    reader.onload = () => {
+      this.imageUrl = reader.result
+      this.file = files[0]
+    }
+  }
+
+  constructor() {}
 }
