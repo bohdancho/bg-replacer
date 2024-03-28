b3ca2aea427f032dfb1540c4336a11cf69d311a8
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-28 11:35:37 +0100

    feat(frontend): add reset button, refactor state

--- frontend/src/app/img-processor/img-processor.component.ts
+++ frontend/src/app/img-processor/img-processor.component.ts
@@ -1,11 +1,19 @@
 import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http'
-import { Component, inject, signal } from '@angular/core'
+import { Component, Signal, effect, inject, signal } from '@angular/core'
 import { FormControl, ReactiveFormsModule } from '@angular/forms'
 import { RouterOutlet } from '@angular/router'
-import { filter, take, map, switchMap, catchError, of, Subject, ignoreElements } from 'rxjs'
+import { filter, map, switchMap, catchError, of, distinctUntilChanged, tap, Subject } from 'rxjs'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { ImgViewComponent } from './img-view.component'
 import { CommonModule } from '@angular/common'
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop'
+
+type ImgProcessorState = {
+  processingEnabled: Signal<boolean | undefined>
+  originalSrc: string | null
+  processedSrc: string | null
+  processingError: string | null
+}
 
 @Component({
   selector: 'app-img-processor',
@@ -15,56 +23,82 @@ import { CommonModule } from '@angular/common'
     class: 'flex-1 flex flex-col justify-center items-center self-stretch gap-4',
   },
   template: `
-    @if (originalImgSrc()) {
+    @if (state().originalSrc) {
       <mat-slide-toggle [formControl]="processingControl">Grayscale</mat-slide-toggle>
       <app-img-view
         [processingEnabled]="(processingControl.valueChanges | async) ?? false"
-        [originalSrc]="originalImgSrc()"
-        [processedSrc]="(processedImgSrc | async) ?? null"
-        [processingErrorMsg]="(processingErrorMsg | async) ?? null"
+        [originalSrc]="state().originalSrc"
+        [processedSrc]="state().processedSrc ?? null"
+        [processingErrorMsg]="state().processingError ?? null"
       />
+      <button mat-raised-button color="primary" (click)="reset()">Reset</button>
     } @else {
-      <input type="file" (change)="onOriginalImgChange($event)" />
+      <input type="file" (change)="onOriginalChange($event)" />
     }
   `,
 })
 export class ImageProcessorComponent {
   readonly http = inject(HttpClient)
 
-  reset = new Subject<void>()
-  onOriginalImgChange(event: Event) {
+  processingControl = new FormControl(false)
+  state = signal<ImgProcessorState>({
+    processingEnabled: toSignal(this.processingControl.valueChanges.pipe(map(Boolean))),
+    originalSrc: null,
+    processedSrc: null,
+    processingError: null,
+  })
+
+  reset() {
+    this.processingControl.setValue(false)
+    this.state.update((state) => ({ ...state, originalSrc: null, processedSrc: null, processingError: null }))
+  }
+
+  onOriginalChange(event: Event) {
     const files = (event.target as HTMLInputElement).files
     if (!files || files.length === 0) return
 
     const reader = new FileReader()
     reader.readAsDataURL(files[0])
     reader.onload = () => {
-      this.originalImgSrc.set(reader.result as string)
+      this.state.update((state) => ({
+        ...state,
+        originalSrc: reader.result as string,
+      }))
     }
   }
-  originalImgSrc = signal<string | null>(null)
 
-  processingControl = new FormControl(false)
-  processingEnabled = signal(() => this.processingControl.value)
   processingRequest$ = this.processingControl.valueChanges.pipe(
+    tap(console.log),
     filter(Boolean),
-    take(1),
-    map(() => this.originalImgSrc()),
-    filter(Boolean),
-    switchMap((img) => this.http.post<string>('api/grayscale', { img })),
+    map(() => this.state().originalSrc),
+    distinctUntilChanged(),
+    switchMap((img) =>
+      this.http.post<string>('api/grayscale', { img }).pipe(
+        catchError((err) => {
+          this.handleProcessingError(err)
+          return of(null)
+        }),
+      ),
+    ),
   )
 
-  processedImgSrc = this.processingRequest$.pipe(catchError(() => of(null)))
-  processingErrorMsg = this.processingRequest$.pipe(
-    ignoreElements(),
-    catchError((err: HttpErrorResponse) => {
-      if (typeof err.error === 'string') {
-        return of(err.error)
-      }
-      if (err.message) {
-        return of(err.message)
-      }
-      return of('Unknown error')
-    }),
-  )
+  handleProcessingError(err: HttpErrorResponse) {
+    let msg = 'Unknown error'
+    if (typeof err.error === 'string') {
+      msg = err.error
+    } else if (err.message) {
+      msg = err.message
+    }
+    this.state.update((state) => ({ ...state, processingError: msg }))
+  }
+
+  constructor() {
+    this.processingRequest$.pipe(takeUntilDestroyed()).subscribe((src) => {
+      return this.state.update((state) => ({ ...state, processedSrc: src }))
+    })
+
+    effect(() => {
+      console.log(this.state())
+    })
+  }
 }
