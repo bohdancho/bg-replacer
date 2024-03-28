1e3f17f19ed066ecde85083cfaa5a767bf69c139
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-28 21:23:15 +0100

    infra(frontend): change prod apiUrl to cloudfront

--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-16-171-141-235.eu-north-1.compute.amazonaws.com/api',
+  apiUrl: 'https://d1qyn4q2029uht.cloudfront.net/api',
 }
