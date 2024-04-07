037d54f09b7599e2c4ebe8ad7df343f84be56f02
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-07 11:51:20 +0200

    fix(backend): inline sql scheme for sqlite to work with docker

--- backend/db/db.go
+++ backend/db/db.go
@@ -11,9 +11,35 @@ type Store struct {
 }
 
 func NewStore() Store {
-	db, err := sql.Open("sqlite3", "db/imaginaer.db")
+	db, err := sql.Open("sqlite3", "./imaginaer.db")
 	if err != nil {
 		panic(err)
 	}
+
+	_, err = db.Exec(scheme)
+	if err != nil {
+		panic(err)
+	}
+
 	return Store{db: db}
 }
+
+var scheme = `
+CREATE TABLE IF NOT EXISTS user (
+  id INTEGER PRIMARY KEY,
+  username STRING NOT NULL UNIQUE,
+  password STRING NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS session (
+  token STRING PRIMARY KEY,
+  expires DATETIME NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE IF NOT EXISTS image (
+  url STRING PRIMARY KEY,
+  owner_id INTEGER NOT NULL UNIQUE,
+  FOREIGN KEY (owner_id) REFERENCES user (id)
+);`
--- backend/db/migrations/image.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE image (
-  url STRING PRIMARY KEY,
-  owner_id INTEGER NOT NULL UNIQUE,
-  FOREIGN KEY (owner_id) REFERENCES user (id)
-);
